371c8b2b4c9bf6e4e2eb1a45acfeb3ce17bcd738
Apoorva-Sati <apoorva.sati.professional@gmail.com>
2026-06-02 06:03:41 +0530

    fixed minor change

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ function App() {
           <div className="md:h-16 border-b border-(--border)">
             <Header />
           </div>
-          <div className="flex-1 overflow-y-auto p-4 md:p-6">
+          <div className="flex-1 overflow-y-auto">
             <Routes>
               <Route path="/" element={<Navigate to="/dashboard" replace />} />
               <Route path="/dashboard" element={<Dashboard />} />
